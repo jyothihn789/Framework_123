d6126424d9ad1aae8f8c989a297b52266db83e32
ft34-213 <chidambarahnbhat@gmail.com>
2025-12-03 12:25:12 +0530

    added new code

--- Qsp2/src/test/java/Execution/Svgtag.java
+++ Qsp2/src/test/java/Execution/Svgtag.java
@@ -9,6 +9,7 @@ public class Svgtag {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		System.out.println("Helloworld");
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("(//*[local-name()='svg'])[1]")).click();
